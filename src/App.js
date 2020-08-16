0eb9feca9029fcb881d5a1dee796c950d43cb96c
M-Ali-Coder <m.ali.coder.93@gmail.com>
2020-08-16 16:47:22 +0200

    Add side nav

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./scss/App.scss";
+import SideMenu from "./components/SideMenu";
+
+function App() {
+  return (
+    <div className="App">
+      <SideMenu />
+    </div>
+  );
+}
+
+export default App;
